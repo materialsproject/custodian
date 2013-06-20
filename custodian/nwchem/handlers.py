51ede89ca17084a76a31fd3330548e6c9ba710fe
__sp__ <shyue@mit.edu>
2013-06-20 10:53:06 -0400

    Even more finetuned fix for cgmin error.

--- custodian/nwchem/handlers.py
+++ custodian/nwchem/handlers.py
@@ -65,7 +65,12 @@ class NwchemErrorHandler(ErrorHandler, MSONable):
                 actions.append("Set noautoz to geometry.")
             elif e == "Bad convergence":
                 nwi = NwInput.from_file(self.input_file)
-                nwi.tasks[self.ntasks - 1].theory_directives["cgmin"] = ""
+                t = nwi.tasks[self.ntasks - 1]
+                t.theory_directives["cgmin"] = ""
+                if t.operation == "optimize":
+                    for t in nwi.tasks:
+                        if t.operation.startswith("freq"):
+                            t.theory_directives["nocgmin"]
                 action = {"_set": {"tasks": [t.to_dict for t in nwi.tasks]}}
                 nwi.write_file(self.input_file)
                 actions.append(action)
