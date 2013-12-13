e9b58d065513071155fee3c13eb7cb9912a30ab5
Xiaohui Qu <xqu@lbl.gov>
2013-12-13 15:02:06 -0800

    test handler with a sucessful file

--- custodian/qchem/test/test_handlers.py
+++ custodian/qchem/test/test_handlers.py
@@ -9,6 +9,8 @@ import os
 import shutil
 from unittest import TestCase
 import unittest
+import sys
+from unittest.case import skip
 from custodian.qchem.handlers import QChemErrorHandler
 
 __author__ = "Xiaohui Qu"
@@ -132,10 +134,19 @@ class QChemErrorHandlerTest(TestCase):
                                         'Molecular charge is not found'],
                              'actions': None})
 
+    def test_no_error(self):
+        shutil.copyfile(os.path.join(test_dir, "hf_no_error.inp"),
+                        os.path.join(scr_dir, "hf_no_error.inp"))
+        shutil.copyfile(os.path.join(test_dir, "hf_no_error.out"),
+                        os.path.join(scr_dir, "hf_no_error.out"))
+        h = QChemErrorHandler(input_file="hf_no_error.inp",
+                              output_file="hf_no_error.out")
+        has_error = h.check()
+        self.assertFalse(has_error)
+
 
     def tearDown(self):
-        #shutil.rmtree(scr_dir)
-        pass
+        shutil.rmtree(scr_dir)
 
 
 
