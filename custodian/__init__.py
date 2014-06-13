056b212726ef58a5f03ebb01515b6883de19c190
__sp__ <shyuep@gmail.com>
2014-06-13 09:44:29 -0700

    Prep v0.7.3 release.

--- custodian/__init__.py
+++ custodian/__init__.py
@@ -1,5 +1,6 @@
-__author__ = "Shyue Ping Ong"
-__date__ = "Jan 3 2013"
-__version__ = "0.7.2"
+__author__ = "Shyue Ping Ong, William Davidson Richards, Stephen Dacek, " \
+             "Xiaohui Qu"
+__date__ = "Jun 13 2014"
+__version__ = "0.7.3"
 
 from custodian import Custodian
