db5136639f0a22b04c8085ddda2c63933aead2ae
William Richards <wmdrichards@gmail.com>
2015-02-06 18:26:42 -0500

    Try to get number of cores from SGE NSLOTS for auto_npar, since multiprocessing.cpu_count() can be misleading for multinode jobs or jobs when the not all cores on the node are assigned to a single job

--- custodian/vasp/jobs.py
+++ custodian/vasp/jobs.py
@@ -155,7 +155,9 @@ class VaspJob(Job):
                         del incar["NPAR"]
                     else:
                         import multiprocessing
-                        ncores = multiprocessing.cpu_count()
+                        # try sge environment variable first
+                        # (since multiprocessing counts cores on the current machine only)
+                        ncores = os.environ.get('NSLOTS') or multiprocessing.cpu_count()
                         for npar in range(int(round(math.sqrt(ncores))),
                                           ncores):
                             if ncores % npar == 0:
