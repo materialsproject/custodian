e6d4754111071fc215c5bb206edf041f537c7e33
__sp__ <ongsp@eng.ucsd.edu>
2016-05-07 10:51:41 -0700

    v1.0.0 update.

--- custodian/__init__.py
+++ custodian/__init__.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 __author__ = "Shyue Ping Ong, William Davidson Richards, Stephen Dacek, " \
              "Xiaohui Qu"
-__date__ = "Sep 10 2015"
-__version__ = "0.8.8"
+__date__ = "May 7 2016"
+__version__ = "1.0.0"
 
 from .custodian import Custodian
