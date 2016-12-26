fbddc0137080846cf31cf23da80bb0c4d6005808
Hanmei Tang <hat003@eng.ucsd.edu>
2016-12-26 13:57:43 -0800

    add unitteest

--- custodian/vasp/tests/test_jobs.py
+++ custodian/vasp/tests/test_jobs.py
@@ -17,10 +17,11 @@ __date__ = "Jun 1, 2012"
 import unittest
 import os
 import shutil
+import glob
 
 import multiprocessing
-from custodian.vasp.jobs import VaspJob
-from pymatgen.io.vasp import Incar
+from custodian.vasp.jobs import VaspJob, VaspNEBJob
+from pymatgen.io.vasp import Incar, Kpoints
 
 test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
                         'test_files')
@@ -71,6 +72,66 @@ class VaspJobTest(unittest.TestCase):
     def test_static(self):
         #Just a basic test of init.
         VaspJob.double_relaxation_run(["vasp"])
+
+
+class VaspNEBJobTest(unittest.TestCase):
+
+    def test_to_from_dict(self):
+        v = VaspNEBJob("hello")
+        v2 = VaspNEBJob.from_dict(v.as_dict())
+        self.assertEqual(type(v2), type(v))
+        self.assertEqual(v2.vasp_cmd, "hello")
+
+    def test_setup(self):
+        if "VASP_PSP_DIR" not in os.environ:
+            os.environ["VASP_PSP_DIR"] = test_dir
+        os.chdir(os.path.join(test_dir, 'setup_neb'))
+
+        v = VaspNEBJob("hello", half_kpts=True)
+        v.setup()
+
+        incar = Incar.from_file("INCAR")
+        count = multiprocessing.cpu_count()
+        if count > 1:
+            self.assertGreater(incar["NPAR"], 1)
+
+        kpt = Kpoints.from_file("KPOINTS")
+        kpt_pre = Kpoints.from_file("KPOINTS.orig")
+        self.assertEqual(kpt_pre.style.name, "Monkhorst")
+        self.assertEqual(kpt.style.name, "Gamma")
+
+        shutil.copy("KPOINTS.orig", "KPOINTS")
+        os.remove("INCAR.orig")
+        os.remove("KPOINTS.orig")
+        os.remove("POTCAR.orig")
+        poscars = glob.glob("[0-9][0-9]/POSCAR.orig")
+        for p in poscars:
+            os.remove(p)
+
+    def test_postprocess(self):
+        neb_outputs = ['INCAR', 'KPOINTS', 'POTCAR', 'vasprun.xml']
+        neb_sub_outputs = ['CHG', 'CHGCAR', 'CONTCAR', 'DOSCAR',
+                           'EIGENVAL', 'IBZKPT', 'PCDAT', 'POSCAR',
+                           'REPORT', 'PROCAR', 'OSZICAR', 'OUTCAR',
+                           'WAVECAR', 'XDATCAR']
+
+        os.chdir(os.path.join(test_dir, 'postprocess_neb'))
+        postprocess_neb = os.path.abspath(".")
+
+        v = VaspNEBJob("hello", final=False, suffix=".test")
+        v.postprocess()
+
+        for f in neb_outputs:
+            self.assertTrue(os.path.isfile('{}.test'.format(f)))
+            os.remove('{}.test'.format(f))
+
+        sub_folders = glob.glob("[0-9][0-9]")
+        for sf in sub_folders:
+            os.chdir(os.path.join(postprocess_neb, sf))
+            for f in neb_sub_outputs:
+                if os.path.exists(f):
+                    self.assertTrue(os.path.isfile('{}.test'.format(f)))
+                    os.remove('{}.test'.format(f))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
