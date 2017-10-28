a25dcce3f1c02b55936bc827a27aa1b369e3b809
shyamd <shyamd@lbl.gov>
2017-10-28 15:09:22 -0700

    fix numpy bug

--- custodian/vasp/handlers.py
+++ custodian/vasp/handlers.py
@@ -622,7 +622,7 @@ class DriftErrorHandler(ErrorHandler):
             return False
         else:
             curr_drift = outcar.data.get("drift", [])[::-1][:self.to_average]
-            curr_drift = np.average([np.norm(d) for d in curr_drift])
+            curr_drift = np.average([np.linalg.norm(d) for d in curr_drift])
             return curr_drift > self.max_drift
 
     def correct(self):
@@ -653,7 +653,8 @@ class DriftErrorHandler(ErrorHandler):
                             "action": {"_set": {"ENAUG": int(incar.get("ENAUG", 1040) * self.enaug_multiply)}}})
 
 
-        curr_drift = np.sum(np.abs(outcar.data.get("drift",[])[::-1][:self.to_average])) / (3 * self.to_average)
+        curr_drift = outcar.data.get("drift", [])[::-1][:self.to_average]
+        curr_drift = np.average([np.linalg.norm(d) for d in curr_drift])
         VaspModder(vi=vi).apply_actions(actions)
         return {"errors": "Excessive drift {} > {}".format(curr_drift, self.max_drift), "actions": actions}
 
