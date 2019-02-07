a7541b52fb7e8afa4736616d61376180fa603241
__sp__ <shyuep@gmail.com>
2019-02-07 15:52:32 -0800

    Update dev docs

--- custodian/__init__.py
+++ custodian/__init__.py
@@ -2,6 +2,6 @@ from __future__ import unicode_literals
 
 __author__ = "Shyue Ping Ong, William Davidson Richards, Stephen Dacek, " \
              "Xiaohui Qu"
-__version__ = "2018.8.10"
+__version__ = "2019.2.7"
 
 from .custodian import Custodian
