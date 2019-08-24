04fd6c71ef66b8b1513fd1d05373b3e17585c1a8
zyrd <shyuep@gmail.com>
2019-08-24 08:53:16 -0700

    Minor fix to authors and codestyle

--- custodian/__init__.py
+++ custodian/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
-__author__ = "Shyue Ping Ong, William Davidson Richards, Stephen Dacek, " \
-             "Xiaohui Qu"
+__author__ = "Shyue Ping Ong, William Davidson Richards, Stephen Dacek, Xiaohui Qu, Samuel M. Blau"
 __version__ = "2019.8.24"
 
 from .custodian import Custodian
