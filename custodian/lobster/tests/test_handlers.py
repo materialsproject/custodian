996513882f94ca44e60b1adfb6c25355ac43b77c
JaGeo <janinegeorge.ulfen@gmail.com>
2020-04-27 15:04:34 +0200

    Add missing empty folder

--- custodian/lobster/tests/test_handlers.py
+++ custodian/lobster/tests/test_handlers.py
@@ -4,7 +4,7 @@ import unittest
 from custodian.lobster.handlers import ChargeSpillingValidator, EnoughBandsValidator, LobsterFilesValidator
 
 # get location of module
-TEST_DIR = os.path.dirname(os.path.abspath(__file__)) 
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 test_files_lobster = os.path.join(TEST_DIR, '../../../test_files/lobster/lobsterouts')
 
 
@@ -23,7 +23,6 @@ class TestChargeSpillingValidator(unittest.TestCase):
         v3 = ChargeSpillingValidator(output_filename=os.path.join(test_files_lobster, "nolobsterout", "lobsterout"))
         self.assertFalse(v3.check())
 
-        # TODO: get non spin polarized lobsterout
         v4 = ChargeSpillingValidator(output_filename=os.path.join(test_files_lobster, "no_spin", "lobsterout"))
         self.assertFalse(v4.check())
 
@@ -49,12 +48,11 @@ class TestLobsterFilesValidator(unittest.TestCase):
         os.chdir(os.path.join(test_files_lobster, "nolobsterout"))
         v2 = LobsterFilesValidator()
         self.assertTrue(v2.check())
-    
-    def test_check_and_correct_3(self): 
+
+    def test_check_and_correct_3(self):
         os.chdir(os.path.join(test_files_lobster, "crash"))
         v2 = LobsterFilesValidator()
         self.assertTrue(v2.check())
-
 
     def test_as_dict(self):
         os.chdir(test_files_lobster)
@@ -62,7 +60,6 @@ class TestLobsterFilesValidator(unittest.TestCase):
         d = v.as_dict()
         v2 = LobsterFilesValidator.from_dict(d)
         self.assertIsInstance(v2, LobsterFilesValidator)
-
 
 
 class TestEnoughBandsValidator(unittest.TestCase):
