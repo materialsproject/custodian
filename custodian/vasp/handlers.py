12e461fe571fdd23b1f60e210d94d53ec1a82211
mwo <m.wolloch@gmail.com>
2021-01-22 10:08:28 +0100

    Updated the check method in the IncorrectSmearingHandler class in custodian.vasp.handlers.py to allow static calculations with ISMEAR = -5 for metals.
    A detailed discussion can be found here:
    https://matsci.org/t/custodian-incorrectsmearinghandler-should-allow-for-total-energy-calculations-for-metals-with-ismear-5/35157

--- custodian/vasp/handlers.py
+++ custodian/vasp/handlers.py
@@ -989,10 +989,10 @@ class UnconvergedErrorHandler(ErrorHandler):
 
 class IncorrectSmearingHandler(ErrorHandler):
     """
-    Check if a calculation is a metal (zero bandgap) but has been run with
-    ISMEAR=-5, which is only appropriate for semiconductors. If this occurs,
-    this handler will rerun the calculation using the smearing settings appropriate
-    for metals (ISMEAR=-2, SIGMA=0.2).
+    Check if a calculation is a metal (zero bandgap), has been run with
+    ISMEAR=-5, and is not a static calculation, which is only appropriate for
+    semiconductors. If this occurs, this handler will rerun the calculation
+    using the smearing settings appropriate for metals (ISMEAR=-2, SIGMA=0.2).
     """
 
     is_monitor = False
@@ -1013,8 +1013,11 @@ class IncorrectSmearingHandler(ErrorHandler):
         """
         try:
             v = Vasprun(self.output_filename)
-            # check whether bandgap is zero and tetrahedron smearing was used
-            if v.eigenvalue_band_properties[0] == 0 and v.incar.get("ISMEAR", 1) < 0:
+            # check whether bandgap is zero, tetrahedron smearing was used
+            # and relaxation is performed.
+            if (v.eigenvalue_band_properties[0] == 0 and
+                v.incar.get("ISMEAR", 1) < -3 and 
+                v.incar.get("NSW", 0) <= 1):
                 return True
         except Exception:
             pass
