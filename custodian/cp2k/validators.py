b8730c5231d388e85055f9fd418acc5762290e97
nwinner <nwinner@berkeley.edu>
2021-06-22 18:23:07 -0700

    Specialized validator for charged defects.

--- custodian/cp2k/validators.py
+++ custodian/cp2k/validators.py
@@ -21,4 +21,21 @@ class Cp2kOutputValidator(Validator):
                 return True
         except:
             return True
-        return False
+
+
+class ChargedDefectValidator(Validator):
+
+    """
+    If a output file is charged and no band gap exists, then the
+    calculation is ill-posed. Meant to be used with a double job
+    with the first being a safety check job.
+    """
+
+    def __init__(self, output_file='cp2k.out'):
+        self.output_file = output_file
+
+    def check(self):
+        o = Cp2kOutput(self.output_file, auto_load=True)
+        if o.band_gap:
+            return False
+        return True
