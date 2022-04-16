dbcb12ad663eea5b3741ccec76d88a99ef18b03a
Andrew <asrosen93@gmail.com>
2022-04-16 15:10:45 -0700

    Fix linting

--- custodian/vasp/tests/conftest.py
+++ custodian/vasp/tests/conftest.py
@@ -6,12 +6,17 @@ import pytest
 
 
 def mock_cpu_count(*args, **kwargs):
-    # Instead of running multiprocessing.cpu_count(), we return a fixed
-    # value during tests
+    """
+    Instead of running multiprocessing.cpu_count(), we return a fixed
+    value during tests
+    """
+
     return 64
 
 
 @pytest.fixture(autouse=True)
 def patch_get_potential_energy(monkeypatch):
-    # Monkeypatch the multiprocessing.cpu_count() function
+    """
+    Monkeypatch the multiprocessing.cpu_count() function
+    """
     monkeypatch.setattr(multiprocessing, "cpu_count", mock_cpu_count)
