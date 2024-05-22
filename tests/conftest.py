6d68858f51ae630bf739f646afcdec40e6075866
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2024-05-22 10:19:57 -0700

    precommit

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ import multiprocessing
 import os
 
 import pytest
-from shutil import copyfileobj
 
 TEST_DIR = os.path.dirname(__file__)
 TEST_FILES = f"{TEST_DIR}/files"
@@ -15,14 +14,15 @@ def _patch_get_potential_energy(monkeypatch) -> None:
     """Monkeypatch the multiprocessing.cpu_count() function to always return 64."""
     monkeypatch.setattr(multiprocessing, "cpu_count", lambda: 64)
 
-def get_gzip_or_unzipped(test_file_or_dir : str) -> str:
-    """ 
+
+def get_gzip_or_unzipped(test_file_or_dir: str) -> str:
+    """
     Return the file or its unzipped version, depending on which one exists.
 
     Running pytest in CI seems to unzip the test files prior to testing.
     To get around this behavior, we return the whichever file exists,
     its gzipped version or the unzipped version.
-    
+
     Args:
         test_file_or_dir (str) : the name of the test file or directory
     Returns:
@@ -31,7 +31,7 @@ def get_gzip_or_unzipped(test_file_or_dir : str) -> str:
     """
     if os.path.isdir(test_file_or_dir):
         return test_file_or_dir
-    
+
     for file_to_test in [test_file_or_dir, test_file_or_dir.split(".gz")[0], test_file_or_dir.split(".GZ")[0]]:
         if os.path.isfile(file_to_test):
             return file_to_test
