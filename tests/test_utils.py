c27e9c16b197c539e58ad3b3c063ef024144f900
Andrew Rosen <asrosen93@gmail.com>
2025-05-12 13:37:23 -0400

    Add a test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
-from custodian.utils import tracked_lru_cache
-
+from custodian.utils import backup, tracked_lru_cache
+from pathlib import Path
+import tarfile
 
 def test_cache_and_clear() -> None:
     n_calls = 0
@@ -25,3 +26,13 @@ def test_cache_and_clear() -> None:
 
     assert some_func(1) == 1
     assert n_calls == 3
+
+def test_backup(tmp_path) -> None:
+    with open(tmp_path / "INCAR", "w") as f:
+        f.write("This is a test file.")
+
+    backup(["INCAR"], directory=tmp_path)
+
+    assert Path(tmp_path / "error.1.tar.gz").exists()
+    with tarfile.open(tmp_path / "error.1.tar.gz", "r:gz") as tar:
+        assert len(tar.getmembers()) > 0
